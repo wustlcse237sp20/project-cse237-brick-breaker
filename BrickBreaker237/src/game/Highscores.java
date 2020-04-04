54068d6a208ac503722d80a0f2a109f46271afe6
Griffin Shaw <gshaw@wustl.edu>
2020-04-04 17:34:20 -0500

    Outlined Highscores class and its tests

---
+++ BrickBreaker237/src/game/Highscores.java
@@ -0,0 +1,7 @@
+package game;
+
+import java.io.File;
+
+public class Highscores {
+	
+}
---
+++ BrickBreaker237/src/game/HighscoresTest.java
@@ -0,0 +1,20 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HighscoresTest {
+	Highscores highscores = new Highscores();
+
+	@Test
+	void testGetAllHighscores() {
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testSaveNewHighscore() {
+		
+	}
+}
