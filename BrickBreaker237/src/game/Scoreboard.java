bf2a79607acd46e1042359f9fa20d9d6573cb43f
Griffin Shaw <gshaw@wustl.edu>
2020-04-04 19:39:45 -0500

    Wrote first version of scoreboard test

--- BrickBreaker237/src/game/Scoreboard.java
+++ BrickBreaker237/src/game/Scoreboard.java
@@ -4,8 +4,26 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class Scoreboard {
+	static String defaultFilePath = "highscores.txt";
+	static ArrayList<Highscore> initialHighscores = new ArrayList<Highscore>() { 
+		{
+			add( new Highscore("The_Breaker", 100));
+			add( new Highscore("AAA", 50));
+			add( new Highscore("BrickNoob", 10));
+		}	
+	};
+			
+	private String fileLocation;
 	private File permanentStorage;
 	private ArrayList<Highscore> highscores;
-
+	
+	public Scoreboard() {
+		// TODO: Either read or initialize default highscore file
+	}
+	
+	public ArrayList<Highscore> getHighscores() {
+		return highscores;
+	}
+	
 	
 }
--- BrickBreaker237/src/game/ScoreboardTest.java
+++ BrickBreaker237/src/game/ScoreboardTest.java
@@ -2,19 +2,67 @@ package game;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
 
 class ScoreboardTest {
-	Scoreboard scoreboard = new Scoreboard();
+	
+	@Test
+	void testDefaultInitialization() {
+		String filePath = Scoreboard.defaultFilePath;
+		Scoreboard scoreboard = new Scoreboard();
+		try {
+			File scoreboardDatabase = new File(filePath);
+			Scanner fileReader = new Scanner(scoreboardDatabase);
+			assertNotNull(fileReader);
+			
+		}
+		catch (FileNotFoundException fileNotFoundException) {
+			fail("Scoreboard file not found");
+		}
+		catch (Exception exception) {
+			String cause = exception.getLocalizedMessage();
+			fail("Failed to due: " + cause );
+		}
+	}
+
 
 	@Test
 	void testGetAllHighscores() {
+		Scoreboard scoreboard = new Scoreboard();
+		ArrayList<Highscore> initialHighscores = Scoreboard.initialHighscores;
+		Integer minimumScoreCount = initialHighscores.size();
+		ArrayList<Highscore> highscores = scoreboard.getHighscores();
+		Integer scoreCount = highscores.size();
 		
-		fail("Not yet implemented");
+		// If developers have played the game,
+		// the file might contain more than the initial scores
+		assertTrue(scoreCount >= minimumScoreCount);
+		
+		for (Highscore score: initialHighscores) {
+			assertTrue(highscores.contains(score));
+		}
+
 	}
 	
 	@Test
 	void testSaveNewHighscore() {
+		Scoreboard scoreboard = new Scoreboard();
+		
+		ArrayList<Highscore> originalScores = scoreboard.getHighscores();
+		Integer originalScoreCount = originalScores.size();
+		
+		Highscore newHighscore = new Highscore("Test", 10);
+		ArrayList<Highscore> updatedScores =  scoreboard.getHighscores();
+		Integer updatedScoreCount = updatedScores.size();
+		
+		assertEquals(originalScoreCount + 1, updatedScoreCount);
+		assertTrue(updatedScores.contains(newHighscore));
+		assertFalse(originalScores.contains(newHighscore));
 		
 	}
 }
