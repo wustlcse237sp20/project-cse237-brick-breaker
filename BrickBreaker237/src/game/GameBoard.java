3a8e06a9bcc2914aadfa8b87b1c3fbd7078f2490
Eric Oshima <eoshima@wustl.edu>
2020-04-05 16:07:22 -0700

    Quick fix varibles

--- BrickBreaker237/src/game/GameBoard.java
+++ BrickBreaker237/src/game/GameBoard.java
@@ -9,14 +9,14 @@ import java.util.concurrent.TimeUnit;
 public class GameBoard extends JPanel {
 
 	private static final int FPS = 60;
-	public BreakableBrick breakableBricks[][];
-	public int initalBrickHealth = 3;
-	public int brickHeight;
-	public int brickWidth;
-	public int brickRow;
-	public int brickCol;
-	public int boardDim;
-	public boolean gameOver = false;
+	private BreakableBrick breakableBricks[][];
+	private int initalBrickHealth = 3;
+	private int brickHeight;
+	private int brickWidth;
+	private int brickRow;
+	private int brickCol;
+	private int boardDim;
+	private boolean gameOver = false;
 
 	public GameBoard(int boardDim, int brickRow, int brickCol) {
 		this.brickRow = brickRow;
@@ -27,6 +27,9 @@ public class GameBoard extends JPanel {
 		breakableBricks = initBricks(brickCol, brickRow);
 	}
 
+	/**
+	 * Manage refreshing graphics, called with repaint()
+	 */
 	protected void paintComponent(Graphics g) {
 		setBackground(Color.black);
 		drawBreakableBricks((Graphics2D) g);
@@ -40,19 +43,32 @@ public class GameBoard extends JPanel {
 		executor.scheduleAtFixedRate(new Runnable() {
 			@Override
 			public void run() {
-				// update game here
+				if (!gameOver) {
+					// update game here
 
-				// repaint refreshes graphics
-				repaint();
+					// repaint refreshes graphics
+					repaint();
+				} else {
+					// game over state
+					
+				}
 			}
 		}, 0, 1000 / FPS, TimeUnit.MILLISECONDS);
 	}
 
+	/**
+	 * init game board of bricks
+	 * 
+	 * @param col column brick is in
+	 * @param row row brick is in
+	 * @return
+	 */
 	public BreakableBrick[][] initBricks(int col, int row) {
 		BreakableBrick brickArray[][] = new BreakableBrick[col][row];
 		for (int i = 0; i < col; i++) {
 			for (int j = 0; j < row; j++) {
-				brickArray[i][j] = new BreakableBrick(i, j, brickWidth , brickHeight, i * brickHeight, j * brickWidth,initalBrickHealth);
+				brickArray[i][j] = new BreakableBrick(i, j, brickWidth, brickHeight, i * brickHeight, j * brickWidth,
+						initalBrickHealth);
 			}
 		}
 		return brickArray;
@@ -61,31 +77,31 @@ public class GameBoard extends JPanel {
 	/**
 	 * Redraw/update graphics for breakable bricks
 	 * 
-	 * @param g
+	 * @param g Graphics2D component
 	 */
 	public void drawBreakableBricks(Graphics2D g) {
 		for (int i = 0; i < breakableBricks.length; i++) {
 			for (int j = 0; j < breakableBricks[0].length; j++) {
 				BreakableBrick brick = breakableBricks[i][j];
-				int health= breakableBricks[i][j].getHealth();
+				int health = breakableBricks[i][j].getHealth();
 				// if brick still has health, repaint it
 				switch (health) {
-				  case 0:
+				case 0:
 					g.setColor(Color.BLACK);
-				    break;
-				  case 1:
+					break;
+				case 1:
 					g.setColor(Color.YELLOW);
-				    break;
-				  case 2:
-					  g.setColor(Color.ORANGE);
-				    break;
-				  case 3:
+					break;
+				case 2:
+					g.setColor(Color.ORANGE);
+					break;
+				case 3:
 					g.setColor(Color.GREEN);
-				    break;
+					break;
 				}
 				g.fillRect(i * brickHeight, j * brickWidth, brickHeight, brickWidth);
-				
-				//draw outline
+
+				// draw outline
 				g.setColor(Color.BLACK);
 				g.drawRect(i * brickHeight, j * brickWidth, brickHeight, brickWidth);
 
