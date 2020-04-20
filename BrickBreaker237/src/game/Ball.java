090d386a14419c9de6d0e4b881793689582a293c
Vijeth Kaza <54551937+vjkaza22@users.noreply.github.com>
2020-04-20 02:52:48 -0400

    Update Ball.java
    allowed for ball to change colors

--- BrickBreaker237/src/game/Ball.java
+++ BrickBreaker237/src/game/Ball.java
@@ -1,95 +1,100 @@
 package game;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Color;
 
-public class Ball 
+public class Ball
 {
     private int posX;
     private int posY;
     private int dirX;
     private int dirY;
     public Color color;
-    public int damage = 1;
-    
-    private int length = 20;
-    private int height = 20;
+    public int damage;
+    private int length;
+    private int height;
+    private int initX;
+    private int initY;
+    private int initDirX;
+    private int initDirY;
+    ArrayList<Color> possibleBallColors = new ArrayList<Color>( Arrays.asList(Color.RED, Color.BLACK, Color.BLUE, Color.CYAN, Color.GRAY, Color.GREEN, Color.MAGENTA, Color.ORANGE));
 
-    private int initX = 240;
-    private int initY = 430;
-    private int initDirX = 3;
-    private int initDirY = -3;
-
-    public Ball()
+    
+    public Ball() {
+        this.damage = 1;
+        this.length = 20;
+        this.height = 20;
+        this.initX = 240;
+        this.initY = 430;
+        this.initDirX = 3;
+        this.initDirY = -3;
+        this.posX = this.initX;
+        this.posY = this.initY;
+        this.dirX = this.initDirX;
+        this.dirY = -this.initDirY;
+        this.color = Color.RED;
+    }
+	
+    public void changeColor(Color specificColor)
     {
-	    	posX = initX;
-	    	posY = initY;
-	    	dirX = initDirX;
-	    	dirY = -initDirY;
-	    	color = Color.RED;
+	    if(possibleBallColors.contains(specificColor))
+	    {
+	    	myBallColor = specificColor;
+	    }
+	    else
+	    {
+	    	myBallColor = Color.BLACK;
+	    }
     }
     
-    public int getX()
-    {
-    		return posX;
+    public int getX() {
+        return this.posX;
     }
     
-    public int getY()
-    {
-    		return posY;
+    public int getY() {
+        return this.posY;
     }
     
-    public int getHeight()
-    {
-    		return height;
+    public int getHeight() {
+        return this.height;
     }
     
-    public int getLength()
-    {
-    		return length;
+    public int getLength() {
+        return this.length;
     }
     
-    public int getXdir()
-    {
-    		return dirX;
+    public int getXdir() {
+        return this.dirX;
     }
     
-    public int getYdir()
-    {
-    		return dirY;
+    public int getYdir() {
+        return this.dirY;
     }
     
-    public void setXdir(int newXdir)
-    {
-    		dirX = newXdir;
+    public void setXdir(final int dirX) {
+        this.dirX = dirX;
     }
     
-    public void setYdir(int newYdir)
-    {
-    		dirY = newYdir;
+    public void setYdir(final int dirY) {
+        this.dirY = dirY;
     }
     
-    public void updatePos()
-    {
-    		posX += dirX;
-    		posY += dirY;
+    public void updatePos() {
+        this.posX += this.dirX;
+        this.posY += this.dirY;
     }
     
-    public void applyPowerUp(PowerUpBrick powerUp) {
-    	PowerUpType type = powerUp.getType();
-    	switch(type) {
-	    	case DAMAGE: {
-	    		damage = damage * powerUp.getMultiplier();
-	    	}
-		default:
-			break;
-    	}
-    	
+    public void applyPowerUp(final PowerUpBrick powerUpBrick) {
+        switch (Ball.Ball$1.$SwitchMap$game$PowerUpType[powerUpBrick.getType().ordinal()]) {
+            case 1: {
+                this.damage *= powerUpBrick.getMultiplier();
+                break;
+            }
+        }
     }
     
-    public void draw(Graphics g)
-    {
-    		g.setColor(color);
-    		g.fillOval(posX, posY, length, height);
+    public void draw(final Graphics graphics) {
+        graphics.setColor(this.color);
+        graphics.fillOval(this.posX, this.posY, this.length, this.height);
     }
 }
